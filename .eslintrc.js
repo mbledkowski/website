bba86a3d7e195b276a0b4f351504d574b0f2e3f2
Maciej Błędkowski <23295125+mbledkowski@users.noreply.github.com>
2021-10-27 04:18:40 +0200

    .eslintrc.js updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'max-len': 0,
-    semi: [2, 'never'],
+    'arrow-parens': [2, "as-needed"],
+    'semi': [2, 'never'],
   },
 }
