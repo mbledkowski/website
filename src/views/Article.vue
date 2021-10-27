1614d8930284599165e4c76657e5c29883651679
Maciej Błędkowski <23295125+mbledkowski@users.noreply.github.com>
2021-10-27 04:20:14 +0200

    Article.vue now renders md files from posts folder

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -2,7 +2,7 @@
 div#about.win98popup.shadow
   header.bar
     p Blog - Article
-    router-link(to='/').button.shadow
+    router-link(to='/blog').button.shadow
         svg(xmlns='http://www.w3.org/2000/svg' width='8px' height='7px' viewbox='0 0 8 7' fill-rule='evenodd' stroke-linejoin='round' stroke-miterlimit='2')
           path(d='M1 6V5h1V4h1V3h2v1h1v1h1v1h1v1H6V6H5V5H3v1H2v1H0V6h1zm0-4V1H0V0h2v1h1v1h2V1h1V0h2v1H7v1H6v1H2V2H1z')
   main.scroll
@@ -10,16 +10,21 @@ div#about.win98popup.shadow
 </template>
 <script lang="ts">
 import MarkdownIt from 'markdown-it'
-let mdfile = require(`../posts/fluxcloud-serverless-hacklu-2020.md`)
+import { useRoute } from 'vue-router'
 
 const md = new MarkdownIt()
-export default {
-  data() {
-    return {
-      result: md.render(mdfile),
-    }
+
+let mdfile = ''
+
+export default ({
+  setup() {
+    const route = useRoute()
+    fetch(`../posts/${route.params.articleName}.md`).then(response => response.text()).then((element: string) => { mdfile = element })
   },
-}
+  computed: {
+    result() { return md.render(mdfile) },
+  },
+})
 </script>
 <style lang="scss">
 #about section {
