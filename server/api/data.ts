ff6d7008e8c7a9741aa288ea760e59f9f02e931f
mbledkowski <pub@mble.dk>
2025-02-04 23:15:57 +0100

    Fix (hopefully) a bug

--- server/api/data.ts
+++ server/api/data.ts
@@ -1,9 +1,12 @@
 import fs from 'fs';
 import {parse} from 'smol-toml';
+import path from 'path';
+import url from 'url';
 
 export default defineEventHandler((event) => {
   try {
-    const dataRaw = fs.readFileSync('./personal/main.toml');
+    // const dataRaw = fs.readFileSync(path.resolve(__dirname, './personal/main.toml'));
+    const dataRaw = fs.readFileSync(path.relative(process.cwd(), './public/main.toml'));
     const data = parse(dataRaw.toString());
     return data;
   } catch (e) {
