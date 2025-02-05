a8baed0e2a1822781226c7986bdecf25c888e0fd
mbledkowski <pub@mble.dk>
2025-02-05 16:30:55 +0100

    Yet another try at fixing the api/data bug

--- server/api/data.ts
+++ server/api/data.ts
@@ -6,7 +6,7 @@ import url from 'url';
 export default defineEventHandler((event) => {
   try {
     // const dataRaw = fs.readFileSync(path.resolve(__dirname, './personal/main.toml'));
-    const dataRaw = fs.readFileSync(path.relative(process.cwd(), './public/main.toml'));
+    const dataRaw = fs.readFileSync(path.relative(process.cwd(), './public/personal/main.toml'));
     const data = parse(dataRaw.toString());
     return data;
   } catch (e) {
