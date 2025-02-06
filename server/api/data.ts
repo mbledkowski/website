73ba41cc967f55f01e91779d6f027795958f0cdc
mbledkowski <pub@mble.dk>
2025-02-06 14:42:06 +0100

    Remove old code

--- server/api/data.ts
+++ server/api/data.ts
@@ -1,15 +1,7 @@
-// import {parse} from 'smol-toml';
 import data from '~/assets/personal/main.toml';
 
 export default defineEventHandler(async (event) => {
   try {
-    // const dataRaw = await useStorage('assets:server').getItem(`/personal/main.toml`)
-    // console.log(typeof dataRaw)
-    // console.log(dataRaw)
-    // if (dataRaw === null || typeof dataRaw === "number" || !(typeof dataRaw === "string" || "toString" in dataRaw)) {
-    //   throw new Error("Data has no attribute toString");
-    // }
-    // const data = parse(dataRaw.toString());
     return data;
   } catch (e) {
     console.log(e);
